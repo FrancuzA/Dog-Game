cbc7fa68626303cdc6c7009c0fc4e003f192c268
FrancuzA <149786616+FrancuzA@users.noreply.github.com>
2024-04-12 14:40:37 +0200

    addpawsv2

--- Assets/scripts/AddPaws.cs
+++ Assets/scripts/AddPaws.cs
@@ -9,6 +9,8 @@ public class AddPaws : MonoBehaviour
     public Transform playerHead; 
     public float distanceBetweenModules = 1f;
     public Transform player;
+    float springforce = 10f;
+    float damperforce = 3f;
 
     // Update is called once per frame
     void Update()
@@ -23,25 +25,38 @@ public class AddPaws : MonoBehaviour
     void SpawnModule()
     {
       
-        GameObject newModule = Instantiate(modulePrefab, playerHead.position, Quaternion.identity);
-
-       
         Destroy(playerTail.GetComponent<HingeJoint>());
 
        
         Vector3 tailPosition = playerTail.localPosition - playerTail.up * distanceBetweenModules;
         playerTail.localPosition = tailPosition;
 
-       
-        HingeJoint hinge = newModule.AddComponent<HingeJoint>();
+        GameObject newModule = Instantiate(modulePrefab, playerHead.position + new Vector3(0, distanceBetweenModules/2,0), Quaternion.identity);
+
+        HingeJoint hinge = newModule.GetComponent<HingeJoint>();
         hinge.connectedBody = playerHead.GetComponent<Rigidbody>();
+        hinge.anchor = new Vector3 (0,0, -0.5f);
 
         
         HingeJoint tailHinge = playerTail.gameObject.AddComponent<HingeJoint>();
         tailHinge.connectedBody = newModule.GetComponent<Rigidbody>();
-        tailHinge.axis.x = 0;
-        tailHinge.axis.y = 1;
-        
+        tailHinge.axis = new Vector3(0f, 0f, 1f);
+        tailHinge.useSpring = true;
+        tailHinge.useLimits = true;
+
+        JointSpring hingeSpring = tailHinge.spring;
+        hingeSpring.spring = springforce;
+        hingeSpring.damper = damperforce;
+        tailHinge.spring = hingeSpring;
+
+        JointLimits jointLimits = tailHinge.limits;
+        jointLimits.max = 15f;
+        jointLimits.min = -15f;
+        tailHinge.limits = jointLimits;
+
+
+
+
         playerTail = newModule.transform;
 
         newModule.transform.SetParent(player);
