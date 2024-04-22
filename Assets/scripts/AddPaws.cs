05ad0936cd585f70ab5b0648b3c1b563b38ac4ee
FrancuzA <149786616+FrancuzA@users.noreply.github.com>
2024-04-22 23:02:26 +0200

    final paws tweaks

--- Assets/scripts/AddPaws.cs
+++ Assets/scripts/AddPaws.cs
@@ -7,12 +7,13 @@ public class AddPaws : MonoBehaviour
     public GameObject modulePrefab; 
     public Transform playerTail; 
     public Transform playerHead; 
-    public float distanceBetweenModules = 1f;
+    public float distanceBetweenModules =0.33f;
     public Transform player;
     float springforce = 10f;
     float damperforce = 3f;
-    private float drift1 = 0.25f;
-    private float drift2 = -0.1f;
+    public float drift1 =0.41f;
+    public float drift2 = -0.073f;
+
 
     // Update is called once per frame
     void Update()
@@ -29,23 +30,24 @@ public class AddPaws : MonoBehaviour
         if(sklep.HotDogs > 0) {
         Destroy(playerTail.GetComponent<HingeJoint>());
 
-       
-        Vector3 tailPosition = playerTail.localPosition + new Vector3(0, 0f, -0.4f);
+            
+            Vector3 tailPosition = playerTail.localPosition + new Vector3(0, 0f, -distanceBetweenModules);
         playerTail.localPosition = tailPosition;
 
-        GameObject newModule = Instantiate(modulePrefab, playerTail.position + new Vector3(-drift1,0f,drift2), Quaternion.identity);
+        GameObject newModule = Instantiate(modulePrefab, playerTail.position + new Vector3(-drift1,-0.01f,drift2), Quaternion.Euler(0f, 90f,0f));
             
         HingeJoint hinge = newModule.GetComponent<HingeJoint>();
         hinge.connectedBody = playerHead.GetComponent<Rigidbody>();
-        hinge.anchor = new Vector3 (-0.5f,0, 0f);
+        hinge.anchor = new Vector3 (0,0f, 0.046f);
 
         
         HingeJoint tailHinge = playerTail.gameObject.AddComponent<HingeJoint>();
         tailHinge.connectedBody = newModule.GetComponent<Rigidbody>();
         tailHinge.axis = new Vector3(0f, 0f, 1f);
-            tailHinge.anchor = new Vector3(-0.09f, -0.05f, 0);
+        tailHinge.anchor = new Vector3(-0.09f, -0.05f, 0);
         tailHinge.useSpring = true;
         tailHinge.useLimits = true;
+        tailHinge.enablePreprocessing = false;
 
         JointSpring hingeSpring = tailHinge.spring;
         hingeSpring.spring = springforce;
@@ -56,9 +58,6 @@ public class AddPaws : MonoBehaviour
         jointLimits.max = 15f;
         jointLimits.min = -15f;
         tailHinge.limits = jointLimits;
-
-
-
 
         playerHead = newModule.transform;
 
